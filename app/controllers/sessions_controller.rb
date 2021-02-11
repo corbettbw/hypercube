5caae938be0c68b13cceca43a4ef057df41bfbda
Corbett Winningham <Corbett Winningham>
2021-02-11 12:14:08 -0700

    implement basic login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,10 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session] [:email].downcase)
-    if user && user.authenticate(params[:session] [:password])
-      # Log the user in and redirect to the user's show page
+    if user&.authenticate(params[:session] [:password])
+      reset_session
+      log_in user
+      redirect_to user
     else
       #create an error message
       flash.now[:danger] = 'Invalid email/password combination'
@@ -14,5 +16,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
