58eb377699dbfed132bf481cfc2f15db44096d3a
Corbett Winningham <Corbett Winningham>
2021-02-18 16:58:17 -0700

    10.1.4 successful edits with TDD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      reset_session
       log_in @user
       flash[:success] = "Welcome to Hypercube!"
       redirect_to @user
@@ -22,7 +21,16 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
   private
 
   def user_params
