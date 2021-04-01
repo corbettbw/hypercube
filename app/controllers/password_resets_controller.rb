add64305399105651ac32116bc02baebf33102bc
Corbett Winningham <Corbett Winningham>
2021-04-01 15:54:32 -0700

    12.3.3 Adding tests for the password reset function

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -27,9 +27,8 @@ class PasswordResetsController < ApplicationController
       @user.errors.add(:password, "can't be empty")
       render 'edit'
     elsif @user.update(user_params)                     # Case (4)
-      @user.forget
-      reset_session
       log_in @user
+      @user.update_attribute(:reset_digest, nil)
       flash[:success] = "Password has been reset."
       redirect_to @user
     else
